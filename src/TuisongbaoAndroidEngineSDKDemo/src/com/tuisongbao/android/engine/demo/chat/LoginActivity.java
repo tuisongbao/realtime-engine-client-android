6f2e93de240d9088288638200b30dd9e8fcd2bb1
Katherine.Zhu <katherinezhu@augmentum.com.cn>
2015-05-18 17:44:17 +0800

    Move Pub/Sub into a separate activity

--- src/TuisongbaoAndroidEngineSDKDemo/src/com/tuisongbao/android/engine/demo/chat/LoginActivity.java
+++ src/TuisongbaoAndroidEngineSDKDemo/src/com/tuisongbao/android/engine/demo/chat/LoginActivity.java
@@ -3,18 +3,14 @@ package com.tuisongbao.android.engine.demo.chat;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.tuisongbao.android.engine.channel.TSBChannelManager;
-import com.tuisongbao.android.engine.channel.entity.TSBChannel;
 import com.tuisongbao.android.engine.chat.TSBChatManager;
 import com.tuisongbao.android.engine.chat.entity.TSBChatUser;
-import com.tuisongbao.android.engine.common.TSBEngineBindCallback;
 import com.tuisongbao.android.engine.common.TSBEngineCallback;
 import com.tuisongbao.android.engine.demo.R;
 import com.tuisongbao.android.engine.demo.chat.cache.LoginChache;
@@ -24,11 +20,7 @@ public class LoginActivity extends Activity {
     private static String TAG = "com.tuisongbao.android.engine.demo:LoginActivity";
     private EditText mEditTextAccount;
     private Button mButtonLogin;
-
-    private Button mSubscribeChannelButton;
-    private Button mUnsubscribeChannelButton;
-    private EditText mSubscribeChannelEditText;
-    private EditText mUnsubscribeChannelEditText;
+    private Button mPubSubButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +28,7 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
 
         mEditTextAccount = (EditText) findViewById(R.id.login_edittext_account);
-        mButtonLogin = (Button) findViewById(R.id.login_textview_login);
+        mButtonLogin = (Button) findViewById(R.id.login_button_login);
         mButtonLogin.setOnClickListener(new OnClickListener() {
 
             @Override
@@ -80,57 +72,15 @@ public class LoginActivity extends Activity {
             finish();
         }
 
-        mSubscribeChannelButton = (Button) findViewById(R.id.login_button_channel_subscribe);
-        mSubscribeChannelEditText = (EditText) findViewById(R.id.login_edittext_channel_subscribe);
-        mSubscribeChannelButton.setOnClickListener(new OnClickListener() {
+        mPubSubButton = (Button) findViewById(R.id.login_button_pubsub);
+        mPubSubButton.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
-                String channelName = mSubscribeChannelEditText.getText().toString();
-                if (channelName == null || channelName.trim().length() < 1) {
-                    Toast.makeText(LoginActivity.this, "channel name 必须包含有效字符", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                TSBChannel channel = TSBChannelManager.getInstance().subscribePrivateChannel(channelName);
-                channel.bind("engine:subscription_succeeded", new TSBEngineBindCallback() {
-
-                    @Override
-                    public void onEvent(String channelName, String eventName, String data) {
-                        Log.e(TAG, "engine:subscription_succeeded");
-                    }
-                });
-
-                channel.bind("engine:subscription_error", new TSBEngineBindCallback() {
-
-                    @Override
-                    public void onEvent(String channelName, String eventName, String data) {
-                        Log.e(TAG, "engine:subscription_error " + data);
-                    }
-                });
-
-                channel.bind("cool-event", new TSBEngineBindCallback() {
-
-                    @Override
-                    public void onEvent(String channelName, String eventName, String data) {
-                        Log.e(TAG, "cool-event: " + data);
-
-                    }
-                });
-            }
-        });
-
-        mUnsubscribeChannelEditText = (EditText) findViewById(R.id.login_edittext_channel_unsubscribe);
-        mUnsubscribeChannelButton = (Button) findViewById(R.id.login_button_channel_unsubscribe);
-        mUnsubscribeChannelButton.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-                String channelName = mUnsubscribeChannelEditText.getText().toString();
-                if (channelName == null || channelName.trim().length() < 1) {
-                    Toast.makeText(LoginActivity.this, "channel name 必须包含有效字符", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                TSBChannelManager.getInstance().unSubscribe(channelName);
+                Intent intent = new Intent(getApplicationContext(),
+                        PubSubActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
